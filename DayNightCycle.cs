ec0ec20007825a57e979bf4a5ad923da89793160
CodingCompany <122390758+codingcompanygr@users.noreply.github.com>
2023-01-12 00:24:03 +0200

    Update DayNightCycle.cs

--- DayNightCycle.cs
+++ DayNightCycle.cs
@@ -1,3 +1,7 @@
+//to make the script work just add it to any gameobject inside the scene and attach your main light source in the opening.
+//you can adjust how fast the day is passing by chaning the "timePassing" number, the bigger the number the faster the day goes.
+//hope it helped you ;)
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
